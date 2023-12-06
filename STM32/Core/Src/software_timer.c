34c86685e625ea418d8b1303adb19f76c02e902d
LTKNgan <ngan.lathikieu@hcmut.edu.vn>
2023-12-06 19:09:28 +0700

    fix some comment

--- STM32/Core/Src/software_timer.c
+++ STM32/Core/Src/software_timer.c
@@ -9,14 +9,14 @@
 #include "software_timer.h"
 
 //	Array of timer variable
-//	timer_counter[0]:	main counter with F = 1HZ
+//	timer_counter[0]:	counter for auto mode with F = 1HZ
 //	timer_counter[1]:	counter for blink with F = 2HZ
-//	timer_counter[2]:
-//	timer_counter[3]:
+//	timer_counter[2]:	for pedestrian count down, = 4*timeRed
+//	timer_counter[3]:	counter for buzzer
 
 int timer_counter[NUM_TIMER] = {0};
 int timer_flag[NUM_TIMER] = {0};
-int timer_duration[NUM_TIMER] = {1000, 250, 0, 0};
+int timer_duration[NUM_TIMER] = {1000, 500, 0, 0};
 
 // set timer flag and timer counter
 void setTimer(int duration, int index){
